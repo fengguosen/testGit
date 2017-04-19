cfe49cf3a69999ac228aca9a985f01761d8a038e
fengguosen <feng.guosen@ucsmy.com>
2017-04-19 17:09:02 +0800

    commit

--- src/testGit/Test.java
+++ src/testGit/Test.java
@@ -3,14 +3,9 @@ package testGit;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//CLOUD COMMIT
-		//CLOUD COMMIT3
-		//cloud commit 5
-		//CLOUD COMMIT4
-		
-//cloud commtit last01
-		//CLOUD COMMIT LAST 02	}
+
+
+		}
 
 }
 
